d8eaa06e08481f539736ad569ef341ab1d72709e
Pedro Barros <pedrobarrostech@gmail.com>
2016-12-07 11:43:38 -0200

    Update nmap-beatify.sh

--- nmap-beatify.sh
+++ nmap-beatify.sh
@@ -3,6 +3,6 @@ for ip in $(nmap -sN -p 21,22,80 $(echo "$(ifconfig | grep "inet" | grep "broadc
   ports = "$(cat temp | grep "/tcp" | cut -d"/" -f1 | tr "\n" " "]"
   if [ "$ports" == "" ]; then ports="No port open detected"; fi
   os = "$(cat temp | grep "OS details" | cut -d":" -f2 | cut -d" " -f2-99 | cut -d"," -f1 | grep -v "many fingerprints")"
-  if [ "$os" = ""] then; os="No os detected"; fi
+  if [ "$os" == ""] then; os="No os detected"; fi
   echo "IP active: $ip | Ports: $ports | OS: $os"
 done
